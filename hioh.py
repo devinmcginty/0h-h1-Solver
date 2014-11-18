3a4c98d748aa3bc29671aa972a5b59ec5358df12
Devin McGinty <devin.lee.mcginty@gmail.com>
2014-11-17 19:30:44 -0500

    Clean up algorithms and add comments

--- hioh.py
+++ hioh.py
@@ -1,4 +1,8 @@
 from math import sqrt
+# Rules:
+#   1. No more than 2 of a color in a row / column
+#   2. Equal number of color A and B in a row / column
+#   3. No two duplicate rows / columns
 
 COLORS = {
     # There's probably a better way to do this, but this works for now.
@@ -17,7 +21,13 @@ class Grid(object):
         self.clist = clist
         self.grid = []
         self.genGrid(clist)
+    def __repr__(self):
+        rstring = ""
+        for row in self.grid:
+            rstring += ' '.join([COLORS[i] for i in row]) + "\n"
+        return rstring
     def genGrid(self, clist):
+        # Initialize numeric grid
         row = []
         for c in clist:
             row.append(COLORS[c])
@@ -25,28 +35,29 @@ class Grid(object):
                 self.grid.append(row)
                 row = []
     def isSolved(self):
+        # Check if grid is solved
         for row in self.grid:
             if not isRowSolved(row):
                 return False
         return True
     def solve(self):
-        s = 0
+        # Run solving algorithms
         while not self.isSolved():
-            tempgrid = self.grid[:]
             for i in range(2):
                 self.parseRows()
                 self.checkDupRows()
                 self.transposeGrid()
-            if self.grid == tempgrid:
-                print("Failed")
-                print(self)
-                break
     def parseRows(self):
+        # Check for rules 1 and 2
         gridCopy = []
         for row in self.grid:
-            gridCopy.append(parseRow(row))
+            if isRowSolved(row):
+                gridCopy.append(row)
+            else:
+                gridCopy.append(parseRow(row))
         self.grid = gridCopy[:]
     def transposeGrid(self):
+        # Perform a matrix transposition to check columns
         tGrid = []
         for x in range(self.size):
             tRow = []
@@ -55,21 +66,21 @@ class Grid(object):
             tGrid.append(tRow)
         self.grid = tGrid[:]
     def checkDupRows(self):
+        # chuck rule 3
         for i in range(self.size):
-            if self.grid[i].count(COLORS['-']) == 2:
-                for j in range(self.size):
-                    if i == j:
-                        continue
-                    elif self.grid[j].count(COLORS['-']) == 0:
-                        comparison = compareRows(self.grid[i], self.grid[j])
-                        self.grid[i] = comparison if comparison else self.grid[i]
-    def __repr__(self):
-        rstring = ""
-        for row in self.grid:
-            rstring += ' '.join([COLORS[i] for i in row]) + "\n"
-        return rstring
+            if self.grid[i].count(COLORS['-']) != 2:
+                continue
+            for j in range(self.size):
+                if i == j:
+                    continue
+                elif self.grid[j].count(COLORS['-']) == 0:
+                    comparison = compareRows(self.grid[i], self.grid[j])
+                    self.grid[i] = comparison if comparison else self.grid[i]
 
 def compareRows(incomplete, complete):
+    """Compare an incomplete row to a complete row to check for permutability.
+    Return a complete permutation of 'complete' if 'incomplete' is a
+    permutation of 'complete'. Return False otherwise."""
     retrow = []
     for i in range(len(incomplete)):
         if incomplete[i] == -1 * complete[i]:
@@ -81,9 +92,11 @@ def compareRows(incomplete, complete):
     return retrow
 
 def isRowSolved(row):
+    """Check if row is solved"""
     return COLORS['-'] not in row
 
 def transposeGrid(grid):
+    """Transpose grid using matrix transposition."""
     tgrid = []
     size = len(grid)
     for x in range(size):
@@ -94,34 +107,42 @@ def transposeGrid(grid):
     return tgrid
 
 def fillSolve(row, fillNum):
+    """Fills all blank spaces in 'row' with 'fillNum'"""
     for i in range(len(row)):
         if row[i] == COLORS['-']:
             row[i] = fillNum
     return row
 
 def parseRow(row):
+    """Check first two rules:
+        No more than 2 of same color in a row
+        Equal number of both colors in row
+    """
+    empty = COLORS['-']
     rcopy = row[:]
-    if rcopy.count(COLORS['-']) == 1:
-        blankIndex = rcopy.index(COLORS['-'])
-        rcopy[blankIndex] = -1 * sum(rcopy)
-        return rcopy
-    elif rcopy.count(COLORS['R']) == len(row) // 2:
+    # Rule 2, check if all of one color is filled
+    if rcopy.count(COLORS['R']) == len(row) // 2:
         return fillSolve(rcopy, COLORS['B'])
     elif rcopy.count(COLORS['B']) == len(row) // 2:
         return fillSolve(rcopy, COLORS['R'])
+    # Rule 1, prevent 3 of one color in a row
     for i in range(1, len(row) - 1):
-        if row[i - 1] == COLORS['-'] and row[i] == row[i + 1] != COLORS['-']:
+        if row[i - 1] == empty and row[i] == row[i + 1] != empty:
+            # - C C
             rcopy[i - 1] = -1 * row[i]
-        elif row[i + 1] == COLORS['-'] and row[i - 1] == row[i] != COLORS['-']:
+        elif row[i + 1] == empty and row[i - 1] == row[i] != empty:
+            # C C -
             rcopy[i + 1] = -1 * row[i]
-        elif row[i] == COLORS['-'] and row[i - 1] == row[i + 1] != COLORS['-']:
+        elif row[i] == empty and row[i - 1] == row[i + 1] != empty:
+            # C - C
             rcopy[i] = -1 * row[i - 1]
-    if rcopy == row:
+    # Return if nothing changed in row
+    if rcopy == row or row.count(empty) == 0:
         return rcopy
+    # Recurse self until no more optimizations can be done
     return parseRow(rcopy)
 
 def main():
-    # Currently the program is not identifying the cell at [5,5] as red
     clist = "-R-R-R-R--"
     clist += "--B-R--RR-"
     clist += "-R---R--B-"
